47d8cac1f7a48a2733b85d0aa8c9f4bdea8b7337
zengzehua <cesc@zzh>
2019-06-10 23:18:42 +0800

    新增两个线程辅助类的demo

---
+++ src/main/java/com/jsd/concurrent/CountDownLatch/CountDownLatchDemo.java
@@ -0,0 +1,74 @@
+package com.jsd.concurrent.CountDownLatch;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * CountDownLatch{@link CountDownLatch()}是一个类似于计数器的工具
+ * 里面提供
+ * public void await() throws InterruptedException { }; 调用await()方法的线程会被挂起，它会等待直到count值为0才继续执行
+ * public boolean await(long timeout, TimeUnit unit) throws InterruptedException { }; 和await()类似，
+ * 只不过等待一定的时间后count值还 没变为0的话就会继续执行
+ * public void countDown() { };  //将count值减1
+ *
+ * @author A001007008
+ */
+public class CountDownLatchDemo {
+
+    /**
+     * AQS中共享模式下的具体应用
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException {
+        //创建一个启动的门栓
+        //构造函数中会将值传递给count
+        CountDownLatch startLatch = new CountDownLatch(5);
+        //创建一个finish的门栓
+        CountDownLatch finishLatch = new CountDownLatch(5);
+        //创建一个线程池
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        //创建5个线程验证启动时间
+        for (int i = 0; i <5 ; i++) {
+            executorService.execute(new myRunnable(startLatch,finishLatch));
+        }
+        //调用方法阻塞当前线程，等所有的start线程都完成了就被唤醒
+        startLatch.await();
+        //随机做一些事情
+        for (int i = 0; i <2000 ; i++) {
+
+        }
+        //调用方法阻塞当前线程，等所有的finish线程都完成了就被唤醒
+        finishLatch.await();
+    }
+}
+
+class myRunnable implements Runnable {
+
+    CountDownLatch startLatch;
+
+    CountDownLatch finishLatch;
+
+    public myRunnable(CountDownLatch startLatch, CountDownLatch finishLatch) {
+        this.startLatch = startLatch;
+        this.finishLatch = finishLatch;
+    }
+
+    @Override
+    public void run() {
+        //输出时间实验时间是否同一时间启动和结束的
+        System.out.println(Thread.currentThread()+"准备启动"+ LocalTime.now());
+        //启动门栓计数器-1
+        startLatch.countDown();
+        System.out.println(Thread.currentThread()+"启动成功"+ LocalTime.now());
+        System.out.println(Thread.currentThread()+"准备结束"+ LocalTime.now());
+        //结束门栓计数器-1
+        finishLatch.countDown();
+        System.out.println(Thread.currentThread()+"已经结束"+ LocalTime.now());
+    }
+}
---
+++ src/main/java/com/jsd/concurrent/CountDownLatch/CyclicBarrierDemo.java
@@ -0,0 +1,63 @@
+package com.jsd.concurrent.CountDownLatch;
+
+import java.time.LocalTime;
+import java.util.concurrent.*;
+
+/**
+ * {@link CyclicBarrier}
+ * CyclicBarrier主要的功能在于，在并发的情况下，设置一个回环栅栏
+ * 当所有需要经过栅栏的线程到达栅栏之后，栅栏才会放行
+ * 让线程继续进行下去
+ * 相比于CountDownLatch，栅栏是可以复用的，CountDownLatch只能使用一次
+ * 在CyclicBarrier可以在线程执行所有线程都到达栅栏之后，指定执行一个动作(随意线程)
+ * 同时在使用的时候，CountDownLatch更像是某一线程等待其他线程的执行
+ *
+ * @author A001007008
+ */
+public class CyclicBarrierDemo {
+    /**
+     * 当任意一个栅栏上等待的线程离开了栅栏，栅栏就会被破坏(无效)
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+        //在CyclicBarrier可以在线程执行所有线程都到达栅栏之后，指定执行一个动作
+//        CyclicBarrier cyclicBarrier2 = new CyclicBarrier(4,()->{
+//            System.out.println("123");
+//        });
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(new myRunnable2(cyclicBarrier));
+        }
+        //cyclicBarrier是可以在下面中进行重用的
+//        for (int i = 0; i < 5; i++) {
+//            executorService.execute(new myRunnable2(cyclicBarrier));
+//        }
+    }
+}
+class myRunnable2 implements Runnable {
+
+    CyclicBarrier cyclicBarrier;
+
+    public myRunnable2(CyclicBarrier cyclicBarrier) {
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        //输出时间实验时间是否同一时间启动和结束的
+        System.out.println(Thread.currentThread()+"启动成功"+ LocalTime.now());
+        try {
+            cyclicBarrier.await();
+            //另一种等待，当超时的时候就会不再等待，但是会抛出BrokenBarrierException
+//            cyclicBarrier.await(100,TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread()+"我已经完成了");
+        System.out.println(Thread.currentThread()+"去做别的事情了");
+    }
+}
